4556c9f50aa829c0086ccd650c64b683ed579f3c
DESKTOP-AJK5IRC\Nikolay <bgmaster42@gmail.com>
2022-05-30 02:37:45 +0300

    Finished the home page. Started pathfinding section.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,7 +2,8 @@ import React from "react";
 import Navigation from "../Global Components/Navbar";
 import 'antd/dist/antd.css';
 import './home.css'
-import { Layout,Carousel,Typography,Card, Col, Row,Space } from "antd";
+import { Layout,Carousel,Typography,Card, Col, Row,Space,Button } from "antd";
+import {  Link } from "react-router-dom";
 import Foot from "../Global Components/Footer";
 import carouselImage from './carousel.jpg'
 import bubbleSort from './bubbleSort.gif'
@@ -39,19 +40,21 @@ function Home(){
 <div className="site-card-wrapper" >
   <Row gutter={16}>
       <Col span={8}>
-        <Card title="Pathfinding" bordered={false}>
-        <img src={pathfinding} style={{height:'300px',marginLeft:'20%'}} />
+        <Card title="Pathfinding" bordered={false} className="container">
+        <img src={pathfinding} style={{height:'300px'}} />
         <div className="Text">
             <Paragraph style={{paddingTop:'20px'}}>Pathfinding is the plotting, by a computer application, of the shortest route between two points. It is a more practical variant on solving mazes.</Paragraph>
         </div>
+        <Button type='dashed' align='center'><Link to='/pathfinding'>Click</Link></Button>
         </Card>
       </Col>
       <Col span={8}>
-        <Card title="Sorting" bordered={false}>
-        <img src={bubbleSort} style={{height:'300px',marginLeft:'20%'}} />
+        <Card title="Sorting" bordered={false} className="container">
+        <img src={bubbleSort} style={{height:'300px'}} className="container"/>
         <div className="Text">
             <Paragraph > In computer science, arranging in an ordered sequence is called "sorting". Sorting is a common operation in many applications, and efficient algorithms to perform it have been developed. </Paragraph>
        </div>
+       <Button type='dashed' align='center'><Link to='/sorting'>Click</Link></Button>
         </Card>
       </Col>
       <Col span={8}>
@@ -60,6 +63,7 @@ function Home(){
             <div className="Text">
             <Paragraph style={{paddingTop:'20px'}}> Graph theory is the study of graphs, which are mathematical structures used to model pairwise relations between objects. </Paragraph>
             </div>
+            <Button type='dashed' align='center'><Link to='/graphs'>Click</Link></Button>
         </Card>
       </Col>
     </Row>
